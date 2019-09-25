a7fa1658a9ed383a6f787adc4e261421c830442f
Johnson <jo74705@gmail.com>
2019-09-26 01:53:34 +0800

    Fix 'socketIdsInRoom' function not work

--- src/middlewares/ioCallback.js
+++ src/middlewares/ioCallback.js
@@ -1,60 +1,52 @@
-  module.exports = (io) => {
-    function ioCallback(socket) {
-      console.log(`Socket id: ${socket.id}`)
-      socket.on('join', (roomID, callback) => {
-        console.log('join', roomID)
-        const socketIds = socketIdsInRoom(roomID)
-        console.log(socketIds)
-        callback(socketIds)
-        socket.join(roomID)
-        socket.room = roomID
-      })
+module.exports = (io) => {
+  function ioCallback(socket) {
+    console.log(`Socket id: ${socket.id}`)
+    socket.on('join', async (roomID, callback) => {
+      console.log('join', roomID)
+      const socketIds = await socketIdsInRoom(roomID)
+      callback(socketIds)
+      socket.join(roomID)
+      socket.room = roomID
+    })
 
-      socket.on('exchange', data => {
-        console.log('exchange', data.to)
-        data.from = socket.id
-        const to = io.sockets.connected[data.to]
-        to.emit('exchange', data)
-      })
-      socket.on('declineCalling', roomID => {
-        console.log('declineCalling')
-        io.in(roomID).emit('leave')
-        socketIdsInRoom(roomID).forEach(socketId => {
-          // let currentSocket = io.sockets.connected[socketId]
-          if (io.sockets.connected[socketId]) {
-            io.sockets.connected[socketId].conn.close()
-          }
-
-        })
-      })
-      socket.on('checkRoomIsEmpty', (roomID, callBack) => {
-        console.log('checkRoomIsEmpty')
-        const socketIds = socketIdsInRoom(roomID)
-        callBack(socketIds)
-      })
-      socket.on('disconnect', () => {
-        console.log('disconnect')
-        if (socket.room) {
-          const room = socket.room
-          io.to(room).emit('leave', socket.id)
-          socket.leave(room)
+    socket.on('exchange', data => {
+      console.log('exchange', data.to)
+      data.from = socket.id
+      const to = io.sockets.connected[data.to]
+      to.emit('exchange', data)
+    })
+    socket.on('declineCalling', roomID => {
+      console.log('declineCalling')
+      io.in(roomID).emit('leave')
+      socketIdsInRoom(roomID).forEach(socketId => {
+        if (io.sockets.connected[socketId]) {
+          io.sockets.connected[socketId].conn.close()
         }
+
       })
-    }
-
-    function socketIdsInRoom(roomID) {
-      const socketIds = io.nsps['/'].adapter.rooms[roomID]
-
-      if (socketIds) {
-        const collection = []
-        for (const key in socketIds) {
-          collection.push(key)
-        }
-        return collection
-      } else {
-        return []
+    })
+    socket.on('checkRoomIsEmpty', async (roomID, callBack) => {
+      console.log('checkRoomIsEmpty')
+      const socketIds = await socketIdsInRoom(roomID)
+      callBack(socketIds)
+    })
+    socket.on('disconnect', () => {
+      console.log('disconnect')
+      if (socket.room) {
+        const room = socket.room
+        io.to(room).emit('leave', socket.id)
+        socket.leave(room)
       }
-    }
-
-    return { ioCallback }
+    })
   }
+
+  const socketIdsInRoom = roomID => {
+    return new Promise((resolve => {
+      io.sockets.in(roomID).clients((err, clients) => {
+        resolve(clients)
+      })
+    }))
+  };
+
+  return { ioCallback }
+}
