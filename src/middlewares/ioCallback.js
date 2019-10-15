bf8a790b2ece9ce6f93b1f232cdc4d81b5e477af
Johnson <jo74705@gmail.com>
2019-10-15 13:29:01 +0800

    Send socketId

--- src/middlewares/ioCallback.js
+++ src/middlewares/ioCallback.js
@@ -1,7 +1,7 @@
 module.exports = (io) => {
   function ioCallback(socket) {
     console.log(`Socket id: ${socket.id}`)
-    socket.send(socket.id);
+    socket.emit('connect', socket.id)
     socket.on('join', async (roomID, callback) => {
       console.log('join', roomID)
       const socketIds = await socketIdsInRoom(roomID)
