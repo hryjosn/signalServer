30446ab62ab0e4f936b7842a2f23e540b2059f5c
Johnson <jo74705@gmail.com>
2020-08-03 17:31:54 +0800

    Add comment and remove useless code

--- src/middlewares/ioCallback.js
+++ src/middlewares/ioCallback.js
@@ -1,46 +1,47 @@
 module.exports = (io) => {
-  function ioCallback(socket) {
-    socket.on('join', async (roomID, callback) => {
-      // when the client join the room by roomId
-      const socketIds = await socketIdsInRoom(roomID)
-      callback(socketIds)
-      socket.join(roomID)
-      socket.room = roomID
-    })
-    socket.on('exchange', data => {
-      //exchange the clients' data
-      data.from = socket.id;
-      const to = io.sockets.connected[data.to]
-      to.emit('exchange', data)
-    })
-    socket.on('declineCalling', roomID => {
-      //when client refuse or decline the call
-      io.in(roomID).emit('leave')
-      //emit leave to client socket event
-      socketIdsInRoom(roomID).then(socketIds => {
-        socketIds.forEach(socketId => {
-          if (io.sockets.connected[socketId]) {
-            io.sockets.connected[socketId].conn.close()
-          }
+    function ioCallback(socket) {
+        socket.on('join', async (roomID, callback) => {
+            // when the client join the room by roomId
+            const socketIds = await socketIdsInRoom(roomID)
+            callback(socketIds)
+            socket.join(roomID)
+            socket.room = roomID
         })
-      })
-    })
-    socket.on('disconnect', () => {
-      if (socket.room) {
-        const room = socket.room
-        io.to(room).emit('leave', socket.id)
-        socket.leave(room)
-      }
-    })
-  }
+        socket.on('exchange', data => {
+            //exchange the clients' data (include session description protocol and ice candidate information
+            data.from = socket.id;
+            const to = io.sockets.connected[data.to]
+            to.emit('exchange', data)
+        })
+        socket.on('declineCalling', async roomID => {
+            //when client refuse or decline the call
+            io.in(roomID).emit('leave')
+            //trigger the leave from client side's socket event
+            const socketIds = await socketIdsInRoom(roomID)
+            socketIds.forEach(socketId => {
+                //force to disconnect the socket in the room
+                if (io.sockets.connected[socketId]) {
+                    io.sockets.connected[socketId].conn.close()
+                }
+            })
+        })
 
-  const socketIdsInRoom = roomID => {
-    return new Promise((resolve => {
-      io.sockets.in(roomID).clients((err, clients) => {
-        resolve(clients)
-      })
-    }))
-  };
+        socket.on('disconnect', () => {
+            if (socket.room) {
+                const room = socket.room
+                io.to(room).emit('leave', socket.id)
+                socket.leave(room)
+            }
+        })
+    }
 
-  return { ioCallback }
+    const socketIdsInRoom = roomID => {
+        return new Promise((resolve => {
+            io.sockets.in(roomID).clients((err, clients) => {
+                resolve(clients)
+            })
+        }))
+    };
+
+    return { ioCallback }
 }
